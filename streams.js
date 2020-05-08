a6da624a16bed6c6667d6766ae125cf662d21433
Daniel THIRION <defvs.daniel@gmail.com>
2020-05-08 13:32:22 +0200

    Added streamer schedule and changed home page layout and text

---
+++ streams.js
@@ -0,0 +1,74 @@
+const streams = [
+	{
+		name: "Drop Eighty Four", twitch: "dropeightyfour", start: new Date(Date.UTC(2020, 4, 15, 18)), end: null
+	},
+	{
+		name: "Circuit XL", twitch: "scamcircuit", start: new Date(Date.UTC(2020, 4, 15, 20)), end: null
+	},
+	{
+		name: "Information Tray", twitch: "informationtray", start: new Date(Date.UTC(2020, 4, 15, 22)), end: null
+	},
+	{
+		name: "Catch22", twitch: "nouseforanger", start: new Date(Date.UTC(2020, 4, 16, 0)), end: null
+	},
+	{
+		name: "mOOnhouse", twitch: "moonhouse_avdept", start: new Date(Date.UTC(2020, 4, 16, 2)), end: null
+	},
+	{
+		name: "Escape Velocity", twitch: "escape_velocity_official", start: new Date(Date.UTC(2020, 4, 16, 4)), end: null
+	},
+	{
+		name: "Paul Velocity", twitch: "paulvelocity", start: new Date(Date.UTC(2020, 4, 16, 6)), end: null
+	},
+	{
+		name: "Eric Skiles", twitch: "ericskiles", start: new Date(Date.UTC(2020, 4, 16, 8)), end: new Date(Date.UTC(2020, 4, 16, 10))
+	},
+	{
+		name: "Ampsnd", twitch: "ampsnd", start: new Date(Date.UTC(2020, 4, 16, 12)), end: new Date(Date.UTC(2020, 4, 16, 14))
+	},
+	{
+		name: "DJ Fuzzy Logic", twitch: "dj_fuzzylogic", start: new Date(Date.UTC(2020, 4, 16, 16)), end: null
+	},
+	{
+		name: "defvs Daniel", twitch: "defvs", start: new Date(Date.UTC(2020, 4, 16, 18)), end: null
+	},
+	{
+		name: "Nerubicon", twitch: "nerubicon", start: new Date(Date.UTC(2020, 4, 16, 20)), end: null
+	},
+	{
+		name: "The Librarian", twitch: "fromtheannex", start: new Date(Date.UTC(2020, 4, 16, 22)), end: null
+	},
+	{
+		name: "Mad_Rabbits", twitch: "mad_rabbits", start: new Date(Date.UTC(2020, 4, 17, 0)), end: null
+	},
+	{
+		name: "Tac'ehT", twitch: "djtaceht", start: new Date(Date.UTC(2020, 4, 17, 2)), end: null
+	},
+	{
+		name: "Dan Kushner", twitch: "dan_kushner", start: new Date(Date.UTC(2020, 4, 17, 4)), end: null
+	},
+	{
+		name: "DJ Differential", twitch: "differential_faw", start: new Date(Date.UTC(2020, 4, 17, 6)), end: null
+	},
+	{
+		name: "DJ Murdock", twitch: "dj_murdock", start: new Date(Date.UTC(2020, 4, 17, 8)), end: new Date(Date.UTC(2020, 4, 17, 10))
+	},
+	{
+		name: "Noise Ordinance", twitch: "noiseordinance", start: new Date(Date.UTC(2020, 4, 17, 16)), end: null
+	},
+	{
+		name: "Eradik", twitch: "philthycity", start: new Date(Date.UTC(2020, 4, 17, 18)), end: null
+	},
+	{
+		name: "DJ GoldenChild", twitch: "goldenchildnyc", start: new Date(Date.UTC(2020, 4, 17, 20)), end: null
+	},
+	{
+		name: "Griffin Raphael", twitch: "djgrif8", start: new Date(Date.UTC(2020, 4, 17, 22)), end: null
+	},
+	{
+		name: "Chillz", twitch: "beautflchillz", start: new Date(Date.UTC(2020, 4, 18, 0)), end: null
+	},
+	{
+		name: "Fuzzy Lotus", twitch: "fuzzy_lotus", start: new Date(Date.UTC(2020, 4, 18, 2)), end: new Date(Date.UTC(2020, 4, 18, 4))
+	},
+]
